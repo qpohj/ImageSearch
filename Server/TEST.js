9067a7eeb0680b55aa832ea53e40936f37a39957
qpohj <28758232+qpohj@users.noreply.github.com>
2024-03-17 17:13:45 +0100

    test

--- Server/TEST.js
+++ Server/TEST.js
@@ -1,17 +1,17 @@
 //DATAN SOM SPARAS FRÅN ANVÄNDARE OM BILDER STRUKTUR
 
-const users = [
-    {
-        "user": "Pontus",
-        "favoriteIMG":
+// const users = [
+//     {
+//         "user": "Pontus",
+//         "favoriteIMG":
 
 
-            [
-                {
-                    "title": "Pretty Cow",
-                    "byteSize": 2636474,
-                    "url": "http://......."
-                }
-            ]
-    }
-]
+//             [
+//                 {
+//                     "title": "Pretty Cow",
+//                     "byteSize": 2636474,
+//                     "url": "http://......."
+//                 }
+//             ]
+//     }
+// ]
